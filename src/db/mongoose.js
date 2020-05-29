737afd725de8a1032c3f792bb9df040076aa83d0
Josh <joshpedrys@gmail.com>
2020-05-29 18:09:16 -0400

    removed some comments

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,30 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task_manager_api',
     useCreateIndex: true,
     useFindAndModify: false
 });
-
-// const userOne = new User({
-//     name: 'Josh',
-//     email: 'myemail@mead.io',
-//     password: 'fantasticps'
-// });
-
-// userOne.save().then(() => 
-// {
-//     console.log(userOne);
-// }).catch((error) =>
-// {
-//     console.log('Error', error);
-// })
-
-
-// const taskOne = new Task({
-//     description: 'wash car'
-// });
-
-// taskOne.save().then(() =>
-// {
-//     console.log(taskOne);
-// }).catch((error) =>
-// {
-//     console.log('Error!', error);
-// });
