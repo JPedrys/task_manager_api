d3550b3f12900bc5068c1e63196ce070c063e9e6
Josh <joshpedrys@gmail.com>
2020-05-29 20:17:53 -0400

    removed comments

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,6 @@
 // CRUD create read update delete
 
 const {MongoClient, ObjectID} = require('mongodb');
-
-//const connectionURL =  'mongodb://127.0.0.1:27017';
 const databaseName = 'task_manager';
 
 
@@ -14,18 +12,6 @@ MongoClient.connect(process.env.MONGODB_URL, {useNewURLParser: true}, (error, cl
     }
     
     const db = client.db(databaseName);
-
-    // db.collection('Users').deleteMany(
-    //     {
-    //         age: 25
-    //     }
-    // ).then((result) =>
-    // {
-    //     console.log(result)
-    // }).catch((error) =>
-    // {
-    //     console.log(error);
-    // })
 
     db.collection('Tasks').deleteOne(
         {
